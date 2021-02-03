eb40c3eb3c48d9a9d26db8b96d75df63a75fb32e
Steam-Cracked <78454622+Steam-Cracked@users.noreply.github.com>
2021-02-03 17:57:05 +0000

    Add files via upload

---
+++ assets/files/script.js
@@ -0,0 +1,1 @@
+$(function() { $("#support") .load("assets/support.html"); $("#logo") .load("assets/logo.html"); $("#ads") .load("assets/ads.html"); $("#navigation") .load("assets/navigation.html");});
